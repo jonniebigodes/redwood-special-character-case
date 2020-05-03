3761599c96ce6ed893b69d7d7dc9edcda3c2d9a5
jonniebigodes <jonniebigodes@hotmail.com>
2020-05-03 22:34:29 +0100

    adds link to what a cação is

--- web/src/pages/CaOPage/CaOPage.js
+++ web/src/pages/CaOPage/CaOPage.js
@@ -4,6 +4,14 @@ const CaOPage = () => {
       <h1>CaOPage</h1>
       <h2>technically it should be cacao (it should strip down the ç and ã)</h2>
       <p>Find me in ./web\src\pages\CaOPage\CaOPage.js</p>
+      <p>click the link below for the definition of cação.</p>
+      <a
+        href="https://en.wikipedia.org/wiki/Spiny_dogfish"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Cação
+      </a>
     </div>
   )
 }
